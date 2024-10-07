9d951d1865e15a33a37bf8c1824e6e7f5ca1e1b0
Jakub Wisniewski <kwisniew12@gmail.com>
2024-10-07 14:44:05 +0200

    dodano przykłądy

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 # Dołączenie modułów QApplication, QLabel z pakietu PyQt5.QtWidgets
-from PyQt6.QtWidgets import QApplication, QLabel
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget
 
 # Inicjalizacja okna aplikacji
 app = QApplication([])
 
-# Tworzenie prostego pola tekstowego do wyświetlenia
-label = QLabel("Tekst w pierwszej aplikacji okienkowej")
+# Tworzenie widżetu przechowującego elementy interfejsu (np. pola tekstowe)
+window = QWidget()
 
-# Wyświetlenie pola tekstowego
-label.show()
+# Ustawienie tytułu okna
+window.setWindowTitle('PyQt6 Lab')
+
+# Ustawienie wielkości okna
+window.setGeometry(100, 100, 280, 80)
+
+# Ustawienie pozycji początkowej okna
+window.move(60, 15)
+
+# Tworzenie prostego tekstu do wyświetlenia
+label_1 = QLabel("Tekst z pierwszego okna", parent=window)
+
+# Tworzenie drugiego tekstu do wyświetlenia
+label_2 = QLabel("Tekst z drugiego okna", parent=window)
+
+# Wyświetlenie widżetu
+window.show()
 app.exec()
+
+
+
